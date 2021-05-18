321f56e6932cfc11148f2d95ad18ec5e6d669d4e
Shubham Jaiswal <shubhamjaiswal3417@gmail.com>
2021-05-18 22:03:55 +0530

    first commit

---
+++ textutils/views.py
@@ -0,0 +1,65 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+	return render(request, 'index.html')
+
+def analyze(request):
+	# Get the text
+	djtext = request.POST.get('text', 'default')
+
+	# Check for checkbox values
+	removepunc = request.POST.get('removepunc', 'off')
+	fullcaps = request.POST.get('fullcaps', 'off')
+	newlineremover = request.POST.get('newlineremover', 'off')
+	extraspaceremover = request.POST.get('extraspaceremover', 'off')
+	charcount = request.POST.get('charcount', 'off')
+
+	# Check which checkbox is on
+	if removepunc == 'on':
+		punctuations = '''!()-[]{};:'"\, <>./?@#$%^&*_~'''
+		analyzed = ""
+		for char in djtext:
+			if char not in punctuations:
+				analyzed = analyzed + char
+
+		params = {'purpose':'Punctuation Remover', 'analyzed_text':analyzed}
+		djtext = analyzed
+
+	if fullcaps == 'on':
+		analyzed = ""
+		for char in djtext:
+			analyzed = analyzed + char.upper()
+
+		params = {'purpose':'Change to UPPERCASE', 'analyzed_text':analyzed}
+		djtext = analyzed
+
+	if newlineremover == 'on':
+		analyzed = ""
+		for char in djtext:
+			if char != '\n' and char != '\r':
+				analyzed = analyzed + char
+
+		params = {'purpose':'New Line Remover', 'analyzed_text':analyzed}
+		djtext = analyzed
+
+	if extraspaceremover == 'on':
+		analyzed = ""
+		for index, char in enumerate(djtext):
+			if not(djtext[index] == " " and djtext[index + 1] == " "):
+				analyzed = analyzed + char
+
+		params = {'purpose':'Space Remover', 'analyzed_text':analyzed}
+		djtext = analyzed
+
+	if charcount == 'on':
+		analyzed = 0
+		for char in djtext:
+			analyzed = analyzed + 1
+
+		params = {'purpose':'Character Count', 'analyzed_text':analyzed}
+		
+	if (removepunc != "on" and fullcaps != "on" and newlineremover != "on" and extraspaceremover != "on" and charcount != "on"):
+		return HttpResponse("Error! Please select any operation to get the output")
+
+	return render(request, 'analyze.html', params)	
